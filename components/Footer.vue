669e0f2f4b269cf1981799059ddcf24cbdf42742
artyom <artjhomg@gmail.com>
2024-12-26 23:51:31 +0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -7,7 +7,7 @@
         <footer class="mt-16">
             <div class="container text-center mx-auto">
                 <img class="mx-auto w-10 mix-blend-difference" src="/logo-text-dark.svg" alt="text-logo">
-                <p class="mt-4">est. 2024</p>
+                <p class="mt-4 font-medium text-xs">est. 2024</p>
                 <div class="flex justify-center items-center gap-4 mt-4">
                     <i class="fa-brands fa-instagram"></i>
                     <i class="fa-brands fa-x-twitter"></i>
