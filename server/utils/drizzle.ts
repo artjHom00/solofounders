9d66b916c0672912a1496495580a3b92e071ac87
artyom <artjhomg@gmail.com>
2025-01-17 23:29:36 +0400

    db connection now as a singleton

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -5,12 +5,30 @@ export { sql, eq, and, or } from 'drizzle-orm'
 
 export const tables = schema
 
-export function useDrizzle () {
+// https://github.com/drizzle-team/drizzle-orm/issues/1988
+// Singleton function to ensure only one db instance is created
+const singleton = <Value>(name: string, value: () => Value): Value => {
+  const globalAny: any = global;
+  globalAny.__singletons = globalAny.__singletons || {};
+  
+  if (!globalAny.__singletons[name]) {
+    globalAny.__singletons[name] = value();
+  }
+  
+  return globalAny.__singletons[name];
+}
+
+
+export const createDatabaseConnection = () => {
   if (process.env.DATABASE_URL == null) {
     throw new Error('DATABASE_CONNECTION_URL_NOT_PROVIDED')
   }
 
   return drizzle(process.env.DATABASE_URL, { schema })
+}
+
+export const useDrizzle = () => {
+  return singleton('db', createDatabaseConnection);
 }
 
 export type User = typeof schema.users.$inferSelect
