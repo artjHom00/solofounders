747bfc613a2592a5705cc561c9729b2ba4609709
artyom <artjhomg@gmail.com>
2025-01-18 23:01:40 +0400

    vercel-analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
+<script>
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
 <template>
   <div class="min-h-screen transition-colors bg-light text-dark dark:bg-dark dark:text-light">
+    <Analytics/>
     <NuxtLoadingIndicator />
     <NuxtLayout>
       <NuxtPage />
