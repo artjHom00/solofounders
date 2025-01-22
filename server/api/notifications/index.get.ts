26799939c0edba5b8210f56b8ac06417b75e2b9a
artyom <artjhomg@gmail.com>
2025-01-22 21:03:10 +0400

    restore notifications api endpoint

---
+++ server/api/notifications/index.get.ts
@@ -0,0 +1,9 @@
+import notificationsService from "../../services/notifications"
+
+export default defineEventHandler(async (event) => {
+    try {
+        return notificationsService.notifications
+    } catch (e) {
+      return e
+    }
+  })
