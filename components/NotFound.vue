0a0d76e3ec20e2115b7e82806df54bd82029b9fa
artyom <artjhomg@gmail.com>
2025-02-12 13:12:43 +0300

    404 page

---
+++ components/NotFound.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+    <div class="">
+        <h1 class="text-8xl font-extrabold tracking-tighter">:[</h1>
+        <p class="font-medium mt-8">Page not found, <NuxtLink class="underline" to="/">go back</NuxtLink></p>
+    </div>
+
+</template>
+
+<style></style>
---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup>
+import NotFound from '@/components/NotFound.vue'
+
+definePageMeta({
+    layout: 'default'
+})
+
+useHead(() => ({
+    status: 404,
+    title: "Page not found",
+}))
+
+</script>
+<template>
+    <div class="min-h-screen transition-colors bg-light text-dark dark:bg-dark dark:text-light">
+      <NuxtLoadingIndicator :throttle="100" style="background: #52CA72"/>
+      <NuxtLayout>
+        <NotFound class="pt-32 mb-8"  />
+      </NuxtLayout>
+    </div>
+  </template>
+
+<style></style>
