f3b2231aa6b947a61c3b049c47890e546b3ed5b5
artyom <artjhomg@gmail.com>
2025-03-15 02:32:39 +0400

    analytics integrated

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
+<script lang="ts" setup>
+import { Analytics } from '@vercel/analytics/nuxt'
+</script>
 <template>
   <div class="min-h-screen transition-colors bg-light text-dark dark:bg-dark dark:text-light">
+    <Analytics/>
     <CookiesConsent/>
     <NuxtLoadingIndicator :throttle="100" style="background: #52CA72"/>
     <NuxtLayout>
